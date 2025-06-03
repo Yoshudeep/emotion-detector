c5f95a438e156aa35d895aeb9366983de13b57de
Yoshudeep <yoshudeepsingh@gmail.com>
2025-06-03 17:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from streamlit_webrtc import webrtc_streamer
 
 webrtc_streamer(
-    key="test",
+    key="emotion-detection",
+    video_processor_factory=processor_factory,
     media_stream_constraints={"video": True, "audio": False},
-    async_processing=False,
+    rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },
+    async_processing=True,
 )
