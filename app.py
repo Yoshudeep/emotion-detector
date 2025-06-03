593ed7b10cc3aa1306115838b074ddd4276ecbe0
Yoshudeep <yoshudeepsingh@gmail.com>
2025-06-03 17:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="simple")
+webrtc_streamer(
+    key="test",
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=False,
+)
